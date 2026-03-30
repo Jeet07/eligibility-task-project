0e87318bd1eec3aefd84664caece598fbb9d2812
Jeet07 <jeetrajsah@gmail.com>
2026-03-30 19:31:35 +0530

    Delete eligibility-task-project-main directory

--- eligibility-task-project-main/frontend-1/src/api/auth.js
+++
@@ -1,8 +0,0 @@
-import API from "./axios";
-
-export const signup = (data) => API.post("/signup", data);
-export const login = async (data) => {
-  const res = await API.post("/login", data);
-  localStorage.setItem("token", res.data.access_token);
-  return res.data;
-};
--- eligibility-task-project-main/frontend-1/src/utils/auth.js
+++
@@ -1,7 +0,0 @@
-import { jwtDecode } from "jwt-decode";
-
-export const getRole = () => {
-  const token = localStorage.getItem("token");
-  if (!token) return null;
-  return jwtDecode(token).role;
-};
